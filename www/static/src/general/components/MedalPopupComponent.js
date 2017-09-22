970b3277e2b983310f25de847eefbcba73e3e63c
Nuno Machado <nmiguelmoura@gmail.com>
2017-09-22 18:00:07 +0100

    type: feat - Add medal popup. Complete game 1.

---
+++ www/static/src/general/components/MedalPopupComponent.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Nuno on 22/09/17.
+ */
+'use strict';
+nmm.states.specificStates.components.MedalPopupComponent = class MedalPopupComponent extends PIXI.Container {
+    constructor() {
+        super();
+        this._init();
+    }
+
+    _init() {
+        
+    }
+};
--- www/static/src/specific/app00/components/MedalPopupComponent.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Nuno on 22/09/17.
- */
-'use strict';
